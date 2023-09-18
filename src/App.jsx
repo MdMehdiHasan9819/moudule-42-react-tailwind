5959a495499a7db945f8b7e0c937d0af7dea1ea2
Coder-Mehdi-A2 <mehdihasan9819@gmail.com>
2023-09-18 17:42:22 +0600

    added all simpale file and inistall all file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-blue-600 text-4xl text-center'>Vite + React</h1>
+      <button className="btn btn-primary">Primary</button>
+    </>
+  )
+}
+
+export default App
